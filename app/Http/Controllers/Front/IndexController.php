b5905456aa770bb36cb5b8dd2503ad3d7f0b748d
alamgir <alamgirah607737@gmail.com>
2022-12-27 13:54:27 +0600

    banner show index page

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Subcategory;
 use App\Models\Childcategory;
+use App\Models\Product;
 use DB;
 
 class IndexController extends Controller
@@ -14,7 +15,7 @@ class IndexController extends Controller
     public function index()
     {
         $category=Category::all();
-        // $subcategory=Subcategory::all();
-        return view('frontend.index', compact('category'));
+        $bannerproduct=Product::where('product_slider',1)->latest()->first();
+        return view('frontend.index', compact('category','bannerproduct'));
     }
 }
